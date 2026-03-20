59ede61ebccb4ed78eae0461c0409709d960a054
unknown <geek.com77@gmail.com>
2026-03-20 22:14:19 +0200

    Force reset admin password

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [ -f db_backup.json ]; then
-  python manage.py loaddata db_backup.json
-elif [ "$CREATE_SUPERUSER" ]; then
+if [ "$CREATE_SUPERUSER" ]; then
   python manage.py createsuperuser --no-input || true
+  python manage.py shell -c "from django.contrib.auth import get_user_model; U = get_user_model(); u = U.objects.filter(username='admin').first(); u.set_password('admin1234'); u.save()" || true
 fi
